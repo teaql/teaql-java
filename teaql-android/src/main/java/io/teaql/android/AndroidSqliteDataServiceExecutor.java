3542929b18db57118475e3460318da50d4cbabaf
Philip Z <hello@teaql.io>
2026-09-23 07:22:16 +0800

    fix: propagate Android SQLite schema inspection failures

--- teaql-android/src/main/java/io/teaql/android/AndroidSqliteDataServiceExecutor.java
+++ teaql-android/src/main/java/io/teaql/android/AndroidSqliteDataServiceExecutor.java
@@ -9,7 +9,6 @@ import io.teaql.core.sql.portable.PortableSQLRepository;
 import io.teaql.dataservice.sql.SqlDataServiceExecutor;
 import io.teaql.dataservice.sql.SqlExecutionAdapter;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -79,15 +78,12 @@ public class AndroidSqliteDataServiceExecutor extends SqlDataServiceExecutor {
 
             @Override
             public List<Map<String, Object>> getTableColumns(String tableName) {
-                try {
-                    List<Map<String, Object>> columns = adapter.queryForList("PRAGMA table_info(" + tableName + ")", new Object[0]);
-                    for (Map<String, Object> col : columns) {
-                        col.put("column_name", col.get("name"));
-                    }
-                    return columns;
-                } catch (Exception e) {
-                    return Collections.emptyList();
+                List<Map<String, Object>> columns = adapter.queryForList(
+                        "PRAGMA table_info(" + tableName + ")", new Object[0]);
+                for (Map<String, Object> col : columns) {
+                    col.put("column_name", col.get("name"));
                 }
+                return columns;
             }
         };
 
